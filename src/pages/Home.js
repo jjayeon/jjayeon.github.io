b6a178d2182b8c16567a5ff80547cf976b26997a
jjayeon <autumn.chiu.136@gmail.com>
2022-02-14 11:46:20 -0500

    tidy up homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>
-            <p id="about">
+        <div id="about">
+            <p>
                 Hi, I'm Autumn. Humanities major, self-taught programmer, and
                 avid chess player. If you're here from my resume, check out my
-                coding Projects below; if you're not, how'd you get here? Shoot
+                coding projects below; if you're not, how'd you get here? Shoot
                 me an email to let me know.
             </p>
-            <h1>
+            <h3>
                 <Link to="projects">Projects</Link>
-            </h1>
+            </h3>
+            <Link to="projects">
+                <img
+                    src="https://raw.githubusercontent.com/jjayeon/my-website-2/main/projects.png"
+                    alt="projects"
+                />
+            </Link>
         </div>
     );
 }
