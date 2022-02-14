5c076f0d54aa188910f009f431423e867f9d7cb2
jjayeon <autumn.chiu.136@gmail.com>
2022-02-14 10:46:57 -0500

    fix up copy on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Curabitur vestibulum tortor sapien, sit amet porta neque
-                faucibus dapibus. Fusce feugiat rutrum eros non sagittis. Morbi
-                auctor eros in nisi ullamcorper condimentum ac sed ante.
-                Vestibulum a accumsan felis. Fusce vestibulum, lectus id
-                interdum auctor, ligula augue euismod sapien, eget fermentum
-                odio nibh a diam. Suspendisse sed accumsan urna. Aenean non sem
-                et massa sagittis luctus. Nunc at scelerisque ligula. Duis
-                consequat, arcu sit amet cursus bibendum, lacus augue malesuada
-                quam, eget porta velit ligula non sapien. Vivamus at massa
-                porta, pharetra orci non, malesuada ante. Praesent dignissim
-                tortor at consectetur suscipit. Aenean sed ante et velit dictum
-                dignissim.
+            <p id="about">
+                Hi, I'm Autumn. Humanities major, self-taught programmer, and
+                avid chess player. If you're here from my resume, check out my
+                coding Projects below; if you're not, how'd you get here? Shoot
+                me an email to let me know.
             </p>
-            <p>
-                Ut ac metus tincidunt, sollicitudin justo vitae, rutrum lacus.
-                Nulla facilisi. Praesent accumsan ex nibh, sit amet elementum
-                ante placerat non. Pellentesque consequat ullamcorper magna nec
-                posuere. Proin ac sapien vel urna tempor venenatis ut sit amet
-                ante. Donec enim neque, dapibus non efficitur in, euismod et
-                erat. Ut faucibus neque neque.
-            </p>
+            <h1>
+                <Link to="projects">Projects</Link>
+            </h1>
         </div>
     );
 }
