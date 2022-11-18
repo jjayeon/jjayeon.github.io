9efca544c4b1b3c45a85bab6e34029ec1b9f3831
autumn <autumn.chiu.136@gmail.com>
2022-11-18 15:47:07 -0500

    update Youtube page

--- src/pages/Youtube.js
+++ src/pages/Youtube.js
@@ -4,7 +4,21 @@ export default function Youtube() {
           <p>
             Outside of my professional work, I also run a Youtube channel called Autumn Makes Games:
           </p>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLPixJqdf5pWI176ffs_1C_09eaei2evWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <p>
+            <iframe width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/videoseries?list=PLPixJqdf5pWI176ffs_1C_09eaei2evWQ"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+          </p>
+          <p>
+            After playing games for so long, I began to notice some patterns in their designs, so I decided to create my own games that distill these concepts down to a few core principles.  The result is some simple yet deep card games that my friends and I have greatly enjoyed, and that I want to share with the world.
+          </p>
+          <p>
+            Please feel free to try any of these games at your next house party, casual gathering, or first date.  Just be sure to let them know where you got the idea from. ;)
+          </p>
         </div>
     );
 }
