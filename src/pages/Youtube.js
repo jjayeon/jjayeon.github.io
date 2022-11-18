1515f39c1e5a45cea08587b5c81d446a52d564db
autumn <autumn.chiu.136@gmail.com>
2022-11-18 15:40:19 -0500

    add Youtube embed

--- src/pages/Youtube.js
+++ src/pages/Youtube.js
@@ -4,7 +4,7 @@ export default function Youtube() {
           <p>
             Outside of my professional work, I also run a Youtube channel called Autumn Makes Games:
           </p>
-          
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLPixJqdf5pWI176ffs_1C_09eaei2evWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     );
 }
