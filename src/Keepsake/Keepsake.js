22eed2a55bfe01becbdd852b3acbb2f5d067cf50
autumn <autumn.chiu.136@gmail.com>
2023-03-05 15:43:38 -0500

    add keepsake stuff

--- src/Keepsake/Keepsake.js
+++ src/Keepsake/Keepsake.js
@@ -1,7 +1,12 @@
 export default function Keepsake() {
     return (
         <div>
-          <p>[EDIT] write something nice for keepsake</p>
+          <p><a href="https://keepsakehouse.com">Keepsake House</a> is a volunteer-run organization dedicated to showcasing underrepresented musical talent around NYC. Through roundtable-style concerts and a focus on conversation about our artists’ works, Keepsake House hopes to leave listeners with a deeper love for and understanding of the art made: a keepsake to hold onto. Because of the respective backgrounds of the founders, we are particularly committed to promoting Asian American talent and actively seek out BIPOC and LGTBQ+ artists to be part of our shows.</p>
+          <p>
+            I work with Keepsake House as a blog writer and video producer, reviewing and interviewing the artists who perform with us both to capture the essence of their work in evergreen form and to drive some engagement towards them with a little SEO magic. Here's some of the work I've done with Keepsake, updated sporadically; to stay up-to-date, check out the <a href="https://www.keepsakehouse.com/blog">Keepsake blog</a>, and subscribe to us on <a href="https://www.youtube.com/@keepsakehouse">Youtube</a>!
+          </p>
+          <h3>Keepsake Works</h3>
+          <p><a href="https://www.keepsakehouse.com/blog/keepsake-of-the-week-02-23-23">Keepsake of the Week: “to fall in love with anyone…” by Sofía Campoamor</a></p>
         </div>
     );
 }
